ab40629e718d9850598013eb4b1a1b67a7dbbf2d
Jeremy Greer <jeremy.greer@engageonq.com>
2018-06-25 09:05:35 -0400

    Add Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import todoApp from './reducers'
+const store = createStore(todoApp)
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
---
+++ src/reducers.js
@@ -0,0 +1,5 @@
+const initialState = {}
+const reducers = (state = initialState, action) => {
+  return state
+}
+export default reducers
