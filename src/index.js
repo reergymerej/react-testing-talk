3dc02763c63a934ba6077ee357dd113c7ec82402
Jeremy Greer <jex.grizzle@gmail.com>
2018-07-03 18:06:19 -0400

    Add Redux dev tools

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import todoApp from './reducers'
-const store = createStore(todoApp)
+import reducers from './reducers'
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <Provider store={store}>
